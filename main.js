5323f7c0c47633c543c65cec9d0585927ac31c88
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-09-13 18:40:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,25 +13,15 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then((data) => {
-          const currentIcon = data.currently.icon;
           const date = new Date();
           let minutes = date.getMinutes();
-          if (minutes < 10) {
-            minutes = minutes + " 0";
-          };
-          let currentHour = date.getHours();        
-          let adjustedHour = ((currentHour + 11) % 12) + 1;
-
+          let currentHour = date.getHours();
+          //let adjustedHour = ((currentHour + 11) % 12) + 1;
           let currentDay = date.getDay();
-          let amPM = "";
-          
           const setClock = document.querySelectorAll(".clock");
-          setClock.forEach((setClock) => {
-            setClock.children[0].innerHTML = currentHour;
-            setClock.children[1].innerHTML = ": " + minutes + amPM;
-          });
           const hourlyForcastData = data.hourly.data;
           const weeklyForcastData = data.daily.data;
+          const currentIcon = data.currently.icon;
           let weekdays = [
             "Sunday",
             "Monday",
@@ -41,10 +31,9 @@ window.addEventListener("load", () => {
             "Friday",
             "Saturday",
           ];
-          getNext5Days = (currentDay) => {
-            var next5Days = [];
-            //forEach????
-            for (var i = 1; i <= 7; i++) {
+          getNext7Days = (currentDay) => {
+            var next7Days = [];
+            for (var i = 0; i < 8; i++) {
               let dayNumber = currentDay + i;
               let adjustedDayNumber = 0;
               if (dayNumber >= weekdays.length) {
@@ -53,81 +42,65 @@ window.addEventListener("load", () => {
                 adjustedDayNumber = dayNumber;
               }
               let day = weekdays[adjustedDayNumber];
-              next5Days.push(day);
+              next7Days.push(day);
             }
-            return next5Days;
+            return next7Days;
           };
-          //function to generate list items in dom
+
+          //display current time
+          setClock.forEach((setClock) => {
+            setClock.children[0].textContent = currentHour + ": ";
+            setClock.children[1].textContent = minutes;
+          });
+
+          //function to generate list items
           createListItem = (text) => {
             let li = document.createElement("li");
             li.textContent = text;
             return li;
           };
-          //daily forcast elements
-          let dailyForcast = document.getElementById("daily-forcast");
+
+          //get html elements to append data to
+          const dailyForcast = document.getElementById("daily-forcast");
+          const hrForcast = document.getElementById("hr-forcast");
+          const space = " ------------------";
+          
+          //loop api and create dom elements from api data
           for (let i = 0; i < weeklyForcastData.length; i++) {
             const dailyWind = weeklyForcastData[i].windSpeed;
-            const dailyTemp = weeklyForcastData[i].temperatureHigh;
-            const nextDay = getNext5Days(currentDay);
+            let dailyTemp = weeklyForcastData[i].temperatureHigh;
+            const nextDay = getNext7Days(currentDay);          
             dailyForcast.appendChild(createListItem(nextDay[i]));
             dailyForcast.appendChild(createListItem(dailyWind + "MPH"));
             dailyForcast.appendChild(createListItem(dailyTemp + "F"));
-          }
-          //defines dom parent element to append
-          let hrForcast = document.getElementById("hr-forcast");
-          //loop api and create dom elements
+            dailyForcast.appendChild(createListItem(space));      
+            dailyForcast.style.color = 'blue'  
+          };
           for (let i = 0; i < hourlyForcastData.length; i++) {
             const hrWind = hourlyForcastData[i].windSpeed;
             const hrTemp = hourlyForcastData[i].temperature;
             const hrSummary = hourlyForcastData[i].summary;
-            const space = " ";
-     
-            hrForcast.appendChild(createListItem(currentHour++));  
+            hrForcast.appendChild(createListItem(currentHour++));
             hrForcast.appendChild(createListItem(hrSummary));
             hrForcast.appendChild(createListItem(hrWind + "MPH"));
             hrForcast.appendChild(createListItem(hrTemp + "F"));
             hrForcast.appendChild(createListItem(space));
-          }
+          };
+
+          //trying to style daily forcast elements
+          dayForcastChildren = dailyForcast.children
+          console.log(dailyForcast.children[0])
+
           //change bg for current icon
-          let body = document.querySelector("body");
-          icon = "clear-day";
-          switch (currentIcon) {
-            case "clear-day":
-              body.classList.add("change-bg-sun");
-              break;
-            case "partly-cloudy-day":
-            case "cloudy":
-              body.classList.add("change-bg-cloudy");
-              break;
-            case "clear-night":
-              body.classList.add("clear-night-bg");
-              break;
-            case "rain":
-              body.classList.add("change-bg-rain");
-              break;
-            case "sleet":
-            case "fog":
-            case "snow":
-              body.classList.add("snow-bg");
-              break;
-            case "partly-cloudy-night":
-              body.classList.add("partly-cloudy-night");
-              break;
-            default:
-              body.classList.add("default-bg");
-          }
+          changeBackGround = (icon) => {
+            let body = document.querySelector("body");
+            body.classList.add(icon);
+          };
+          changeBackGround(currentIcon);
         });
     });
   }
-  console.log("loaded");
 });
-
-
-
-
-
-
-
 
 /* 
 appendChildren = (parent, children) => {
@@ -135,9 +108,9 @@ appendChildren = (parent, children) => {
     parent.appendChild(child);
   });
 };
+*/
 
-
-function addZero(n) {
-    return (parseInt(n, 10) < 10 ? "0" : "") + n;
-  }
-  addZero(minutes); */
+/*           function addZero(n) {
+            return (parseInt(n, 10) < 10 ? "0" : "") + n;
+          }
+          addZero(minutes); */
