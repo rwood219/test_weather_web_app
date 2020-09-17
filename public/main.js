9a7080d7135b8d738da59831c0d3f2fc75837f47
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-09-16 22:44:26 -0400

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -8,44 +8,55 @@ window.addEventListener("load", () => {
       const api_url = `weather/${lat},${long}`;
       const response = await fetch(api_url);
       const json = await response.json();
-      console.log(json);
-      data = json;
+      const data = json;
       const date = new Date();
       let minutes = date.getMinutes();
       let currentHour = date.getHours();
-      currentHour >= 12 ? (amPm = " PM") : (amPm = " AM");
+
+      // define current day; returns a number 0 thru 6 ; 0 = Sunday
+      const currentDay = date.getDay();
+
+      //DarkSky Api Data vars; console log data for more detail
+      const hourlyForcastData = data.hourly.data;
+      const weeklyForcastData = data.daily.data;
+      const currentIcon = data.currently.icon;
+
+      //get html elements to append api data
+      const dailyForcast = document.getElementById("daily-forcast");
+      const hrForcast = document.getElementById("hr-forcast");
+
+      //change amPM var for am or pm based on currenthour value
+      changeAmpm = (currentHour) => {
+        currentHour >= 12 ? (amPm = " PM") : (amPm = " AM");
+        return amPm;
+      };
+      console.log(changeAmpm(5))
 
       //converts 24hr to 12hr format
       adjustHour = (hourNum) => {
         hourNum == 0
-          ? (hourNum = 12)
+          ? (hourNum = 12 + amPm)
           : hourNum >= 12
           ? (hourNum = hourNum = ((hourNum + 11) % 12) + 1)
           : (hourNum = hourNum);
         return hourNum;
       };
-      
-      //format minutes
+
+      //format minutes to add a 0 if minutes are < 10
       addZero = (num) => {
         num < 10 ? (num = ":" + "0" + num) : (num = ":" + num);
         return num;
       };
 
-      //display current time
+      //display current time in ui at top of page; its just a dumb clock
       const setClock = document.querySelectorAll(".clock");
       setClock.forEach((setClock) => {
         setClock.children[0].textContent = adjustHour(currentHour);
-        setClock.children[1].textContent = addZero(minutes);
+        setClock.children[1].textContent =
+          addZero(minutes) + changeAmpm(currentHour);
       });
 
-      //DarkSky Api Data
-      const hourlyForcastData = data.hourly.data;
-      const weeklyForcastData = data.daily.data;
-      const currentIcon = data.currently.icon;
-
-      // define current day number
-      const currentDay = date.getDay();
-
+      // weekdays array provides starting point to leverage current day number to adjust value to match weekly data from API
       let weekdays = [
         "Sunday",
         "Monday",
@@ -58,15 +69,13 @@ window.addEventListener("load", () => {
 
       // function to adjust day text in ui for weekly forcast
       getNext7Days = (currentDay) => {
-        var next7Days = [];
-        for (var i = 1; i <= 8; i++) {
+        const next7Days = [];
+        for (let i = 1; i <= 8; i++) {
           let dayNumber = currentDay + i;
           let adjustedDayNumber = 0;
-          if (dayNumber >= weekdays.length) {
-            adjustedDayNumber = dayNumber - weekdays.length;
-          } else {
-            adjustedDayNumber = dayNumber;
-          }
+          dayNumber >= weekdays.length
+            ? (adjustedDayNumber = dayNumber - weekdays.length)
+            : (adjustedDayNumber = dayNumber);
           let day = weekdays[adjustedDayNumber];
           next7Days.push(day);
         }
@@ -80,10 +89,7 @@ window.addEventListener("load", () => {
         return li;
       };
 
-      //get html elements to append data to
-      const dailyForcast = document.getElementById("daily-forcast");
-      const hrForcast = document.getElementById("hr-forcast");
-      //tempory to make ui easier to look at for now
+      //temporary to make ui easier to look at for now
       const space = " ------------------";
 
       //loop api and create dom elements from api data
@@ -95,13 +101,15 @@ window.addEventListener("load", () => {
         dailyForcast.appendChild(createListItem(dailyWind + "MPH"));
         dailyForcast.appendChild(createListItem(dailyTemp + "F"));
         dailyForcast.appendChild(createListItem(space));
-        //dailyForcast.children.style.classList.add('add-border')
       }
       for (let i = 0; i < hourlyForcastData.length; i++) {
         const hrWind = hourlyForcastData[i].windSpeed;
         const hrTemp = hourlyForcastData[i].temperature;
         const hrSummary = hourlyForcastData[i].summary;
+
+        //adjust hour is interfering with ampm text
         hrForcast.appendChild(createListItem(adjustHour(currentHour++)));
+
         hrForcast.appendChild(createListItem(hrSummary));
         hrForcast.appendChild(createListItem(hrWind + "MPH"));
         hrForcast.appendChild(createListItem(hrTemp + "F"));
@@ -117,4 +125,3 @@ window.addEventListener("load", () => {
     });
   }
 });
-
