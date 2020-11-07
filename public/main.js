f434fedf2ec166cf36d92a105314d0a788176e7a
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-11-06 23:02:46 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -14,7 +14,6 @@ window.addEventListener("load", () => {
       let minutes = date.getMinutes();
       let currentHour = date.getHours();
       const currentDay = date.getDay();
-      //temporary to make ui easier to look at for now
       const space = " ------------------";
       const hourlyForcastData = data.hourly.data; //DarkSky Api Data vars; console log data for more detail
       const weeklyForcastData = data.daily.data;
@@ -24,7 +23,6 @@ window.addEventListener("load", () => {
       const hrForcast = document.getElementById("hr-forcast");
       const setClock = document.querySelectorAll(".clock");
   
-      // weekdays array provides starting point to leverage current day number to adjust value to match weekly data from API
       let weekdays = [
         "Sunday",
         "Monday",
@@ -35,13 +33,11 @@ window.addEventListener("load", () => {
         "Saturday",
       ];
 
-      //change amPM var for am or pm based on currenthour value
       changeAmpm = (time) => {
         time > 12 ? (amPm = " PM") : (amPm = " AM");
         return amPm;
       };
 
-      //converts 24hr to 12hr format
       adjustHour = (hourNum, amp) => {
         hourNum == 0
           ? (hourNum = 12)
@@ -51,20 +47,17 @@ window.addEventListener("load", () => {
         return hourNum;  
       };
 
-      //format minutes to add a 0 if minutes are < 10
       addZero = (num) => {
         num < 10 ? (num = ":" + "0" + num) : (num = ":" + num);
         return num;
       };
 
-      //display current time in ui at top of page; its just a dumb clock
       setClock.forEach((setClock) => {
         setClock.children[0].textContent = adjustHour(currentHour);
         setClock.children[1].textContent =
           addZero(minutes) + changeAmpm(currentHour);
       });
 
-      // function to adjust day text in ui for weekly forcast
       getNext7Days = (currentDay) => {
         const next7Days = [];
         for (let i = 1; i <= 8; i++) {
@@ -79,14 +72,12 @@ window.addEventListener("load", () => {
         return next7Days;
       };
 
-      //function to generate list items
       createListItem = (text) => {
         let li = document.createElement("li");
         li.textContent = text;
         return li;
       };
 
-      //loop api and create list items from api data
       for (let i = 0; i < weeklyForcastData.length; i++) {
         const dailyWind = weeklyForcastData[i].windSpeed;
         const dailyTemp = weeklyForcastData[i].temperatureHigh;
@@ -109,7 +100,6 @@ window.addEventListener("load", () => {
         hrForcast.appendChild(createListItem(space));
       }
 
-      //change bg for current icon
       changeBackGround = (icon) => {
         let body = document.querySelector("body");
         body.classList.add(icon);
